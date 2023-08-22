2d80ef6479c85a2bacb625e0552d27d8bff44d13
curoli <curoli@gmail.com>
2023-08-22 19:22:55 -0400

    API draft, part 1.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
 pub mod example;
 
+pub struct Model {}
+
+pub struct Var {
+    name: String,
+}
+
+impl Model {
+    pub fn new() -> Model { Model {} }
+}
+
+impl Var {
+    pub fn new(name: String) -> Var { Var { name } }
+}
