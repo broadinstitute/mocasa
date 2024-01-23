d21e8a62c44564b99882373d988a420c8217c2dd
curoli <curoli@gmail.com>
2024-01-23 10:21:04 -0500

    Fix exact classification math.

--- src/classify/exact.rs
+++ src/classify/exact.rs
@@ -5,12 +5,12 @@ pub(crate) fn calculate_mu(params: &Params, betas: &[f64], ses: &[f64]) -> f64 {
         params.betas.iter().zip(params.sigmas.iter()).zip(betas.iter())
             .zip(ses.iter())
             .map(|(((&beta, &sigma), &o), &se)| {
-                beta * o / (sigma + se)
+                beta * o / (sigma.powi(2) + se.powi(2))
             }).sum();
     let denominator: f64 =
         params.betas.iter().zip(params.sigmas.iter()).zip(ses.iter())
             .map(|((&beta, &sigma), &se)| {
-                beta.powi(2) / (sigma + se)
+                beta.powi(2) / (sigma.powi(2) + se.powi(2))
             }).sum();
     numerator / denominator
 }
