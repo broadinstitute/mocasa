d111ee55d501c3d605f1d594a4dd1a41fecc2602
curoli <curoli@gmail.com>
2024-02-02 15:38:52 -0500

    Scripts to install and submit to UGER.

---
+++ sh/classify_qsub.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dir=$(pwd)
+
+qsub -b y -l h_rt=4:00:00 -pe smp 4 -R y -binding linear:4  -o "$dir" \
+    /humgen/diabetes2/users/oliverr/pub/bin/mocasa classify -f "$dir"/config.toml  \
---
+++ sh/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cargo install --path . --root /humgen/diabetes2/users/oliverr/pub/
