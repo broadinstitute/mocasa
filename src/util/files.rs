ed499fd52c25399a2cb23eb132b2b49598a3bf0e
curoli <curoli@gmail.com>
2024-02-13 19:29:56 -0500

    Fix parent directory check.

--- src/util/files.rs
+++ src/util/files.rs
@@ -3,7 +3,7 @@ use crate::error::Error;
 
 pub(crate) fn check_parent_dir_exists(path: &str) -> Result<(), Error> {
     if let Some(parent) = Path::new(path).parent() {
-        if !parent.exists() {
+        if parent != Path::new("") && !parent.exists() {
             Err(Error::from(format!("File {} does not exist", parent.display())))?
         }
     };
