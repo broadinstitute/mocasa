059aa0018105a21acbc69d8ede13cf9b1f57e26d
curoli <curoli@gmail.com>
2024-04-22 18:04:20 -0400

    Adding option to run classification in parallel.

--- sh/classify_multi_qsub.sh
+++ sh/classify_multi_qsub.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-run_script=$0
+run_script=run_script=$(readlink -f "$0")
+
 dir=$(pwd)
 n_chunks=10
 
 if [[ $SGE_TASK_ID ]]; then
-  /humgen/diabetes2/users/oliverr/pub/bin/mocasa classify -f "$dir"/config.toml -x $n_chunks -k "$SGE_TASK_ID" "$@"
+  /humgen/diabetes2/users/oliverr/mocasa/multendo/bin/mocasa classify -f "$dir"/config.toml -x $n_chunks -k "$SGE_TASK_ID" "$@"
 else
   qsub -b y -l h_rt=10:00:00 -pe smp 4 -R y -binding linear:4  -cwd -o "$dir" -t 1-$n_chunks "$run_script" "$@"
 fi
